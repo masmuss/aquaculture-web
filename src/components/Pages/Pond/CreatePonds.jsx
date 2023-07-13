d7cc8fb7402bbcd879fb74a480f7ced510e161af
AinulM02 <janulm02@gmail.com>
2023-07-13 19:57:49 +0700

    add: create-ponds

--- src/components/Pages/Pond/CreatePonds.jsx
+++ src/components/Pages/Pond/CreatePonds.jsx
@@ -1,77 +1,121 @@
-import axios from "axios";
-import { useState } from "react"
-import { useNavigate } from "react-router-dom";
-import { authenticatedApi } from "../../../services/api";
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Cookies } from 'react-cookie'
 
 const CreatePonds = () => {
+	let navigate = useNavigate()
+	const [input, setInput] = useState({
+		hardware_id: '',
+		name: '',
+		address: ''
+	})
 
-    let navigate = useNavigate();
-    const [input, setInput] = useState({
-        hardware_id: "",
-        name: "",
-        address: "",
-    });
+	const handleInput = e => {
+		let name = e.target.name
+		let value = e.target.value
 
-    const handleInput = (e) => {
-        let name = e.target.name;
-        let value = e.target.value;
+		if (name === 'hardware_id') {
+			setInput({ ...input, hardware_id: value })
+		} else if (name === 'name') {
+			setInput({ ...input, name: value })
+		} else if (name === 'address') {
+			setInput({ ...input, address: value })
+		}
+	}
 
-        if(name === "hardware_id"){
-            setInput({...input, hardware_id: value});
-        } else if(name === "name"){
-            setInput({...input, name: value});
-        } else if(name === "address"){
-            setInput({...input, address: value});
-        }
-    }
+	const handleSubmit = e => {
+		e.preventDefault()
+		const cookies = new Cookies()
+		const token = cookies.get('token')
+		let { hardware_id, name, address } = input
+		axios
+			.post(
+				'http://127.0.0.1:8000/api/ponds',
+				{
+					hardware_id,
+					name,
+					address
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${token}`
+					}
+				}
+			)
+			.then(res => {
+				console.log(res)
+				navigate('/ponds')
+			})
+			.catch(err => {
+				console.log(err)
+			})
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+		setInput({
+			hardware_id: '',
+			name: '',
+			address: ''
+		})
+	}
 
-        let {hardware_id, name, address} = input;
-        axios.post("http://127.0.0.1:8000/api/ponds", {
-            hardware_id, name, address
-        }).then((res) => {
-            console.log(res);
-            navigate('/ponds');
-        }).catch((err) => {
-            console.log(err);
-        });
+	return (
+		<>
+			<div className="rounded-lg border border-gray-200 bg-white p-6 shadow">
+				<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+					Tambahkan Ponds
+				</h5>
+				<form onSubmit={handleSubmit}>
+					<div className="mb-6">
+						<label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+							Hardware ID
+						</label>
+						<input
+							type="text"
+							className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+							name="hardware_id"
+							value={input.hardware_id}
+							onChange={handleInput}
+						/>
+					</div>
 
-        setInput({
-            hardware_id: "",
-            name: "",
-            address: "",
-        })
-    }
+					<div className="mb-6">
+						<label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+							Kolam
+						</label>
+						<input
+							type="text"
+							className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+							name="name"
+							value={input.name}
+							onChange={handleInput}
+						/>
+					</div>
 
-  return (
-    <>
-        <div className="p-6 bg-white border border-gray-200 rounded-lg shadow">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Tambahkan Ponds</h5>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-6">
-                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hardware ID</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" name="hardware_id" value={input.hardware_id} onChange={handleInput}/>
-                </div>
+					<div className="mb-6">
+						<label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+							Alamat
+						</label>
+						<input
+							type="text"
+							className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+							name="address"
+							value={input.address}
+							onChange={handleInput}
+						/>
+					</div>
 
-                <div className="mb-6">
-                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kolam</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" name="name" value={input.name} onChange={handleInput}/>
-                </div>
-
-                <div className="mb-6">
-                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat</label>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" name="address" value={input.address} onChange={handleInput}/>
-                </div>
-
-                <div className="flex justify-end">
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
-                </div>
-            </form>
-        </div>
-    </>
-  )
+					<div className="flex justify-end">
+						<button
+							type="submit"
+							className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto"
+						>
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
+		</>
+	)
 }
 
 export default CreatePonds
--- src/components/Pages/Pond/index.jsx
+++ src/components/Pages/Pond/index.jsx
@@ -1,10 +1,31 @@
 import { Card } from 'flowbite-react'
 import { usePonds } from '../../../hooks/ponds'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { HiQrcode, HiLocationMarker, HiPencil } from 'react-icons/hi'
+import { HiArchiveBoxArrowDown } from 'react-icons/hi2'
+import axios from 'axios'
+import { Cookies } from 'react-cookie'
 
 const Pond = () => {
+	let navigate = useNavigate()
 	const { ponds, loading, error } = usePonds()
+
+	const deletePonds = async id => {
+		const cookies = new Cookies()
+		const token = cookies.get('token')
+		// eslint-disable-next-line no-undef
+		res = await axios
+			.delete(`http://127.0.0.1:8000/api/ponds/${id}`, {
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			})
+			.then(res => {
+				console.log(res)
+				navigate('/ponds')
+			})
+			.catch(err => console.log(err))
+	}
 
 	if (error) {
 		return <p>{error}</p>
@@ -12,52 +33,66 @@ const Pond = () => {
 
 	return (
 		<>
-		<div className="flex justify-end">
-			<Link to="/ponds/create" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Tambah</Link>
-		</div>
-		
+			<div className="flex justify-end">
+				<Link
+					to="/ponds/create"
+					type="button"
+					className="mb-2 mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				>
+					Tambah
+				</Link>
+			</div>
+			<div className="grid-rows-auto grid grid-cols-4 gap-4">
+				{loading ? (
+					<p>Loading...</p>
+				) : (
+					ponds.map(pond => (
+						<Link to={`/ponds/${pond.hardware_id}`} key={pond.id}>
+							<Card className="w-full border-2 border-gray-200 transition-all hover:bg-gray-100">
+								<div className="flex items-center justify-between gap-3">
+									<div className="title flex gap-3  text-2xl font-bold tracking-wide text-gray-700 dark:text-white">
+										<HiQrcode
+											size="1.5em"
+											className="text-gray-800"
+										/>
+										<p>{pond.hardware_id}</p>
+									</div>
+									<HiArchiveBoxArrowDown
+										className="text-2xl text-red-600 hover:text-red-700"
+										onClick={() =>
+											window.confirm('yakin deck?')
+												? deletePonds(pond?.id)
+												: ''
+										}
+									/>
+								</div>
 
-		<div className="grid-rows-auto grid grid-cols-4 gap-4">
-			{loading ? (
-				<p>Loading...</p>
-			) : (
-				ponds.map(pond => (
-					<Link to={`/ponds/${pond.hardware_id}`} key={pond.id}>
-						<Card className="w-full border-2 border-gray-200 transition-all hover:bg-gray-100">
-							<h5 className="flex items-center gap-3 text-2xl font-bold tracking-wide text-gray-700 dark:text-white">
-								<HiQrcode
-									size="1.5em"
-									className="text-gray-800"
-								/>
-								<p>{pond.hardware_id}</p>
-							</h5>
-							<ul className="space-y-3">
-								<li className="flex items-center space-x-3">
-									<span className="flex items-center gap-2 text-base font-normal leading-tight text-gray-700 dark:text-gray-400">
-										<HiPencil
-											className="text-gray-800"
-											size="1.2em"
-										/>
-										{pond.name}
-									</span>
-								</li>
-								<li className="flex items-center space-x-3">
-									<span className="flex items-center gap-2 text-base font-normal leading-tight text-gray-700 dark:text-gray-400">
-										<HiLocationMarker
-											className="text-gray-800"
-											size="1.2em"
-										/>
-										{pond.address}
-									</span>
-								</li>
-							</ul>
-						</Card>
-					</Link>
-				))
-			)}
-		</div>
+								<ul className="space-y-3">
+									<li className="flex items-center space-x-3">
+										<span className="flex items-center gap-2 text-base font-normal leading-tight text-gray-700 dark:text-gray-400">
+											<HiPencil
+												className="text-gray-800"
+												size="1.5em"
+											/>
+											{pond.name}
+										</span>
+									</li>
+									<li className="flex items-center justify-between space-x-3">
+										<span className="flex items-center gap-2 text-base font-normal leading-tight text-gray-700 dark:text-gray-400">
+											<HiLocationMarker
+												className="text-gray-800"
+												size="1.2em"
+											/>
+											{pond.address}
+										</span>
+									</li>
+								</ul>
+							</Card>
+						</Link>
+					))
+				)}
+			</div>
 		</>
-		
 	)
 }
 
